43ab70c588cdd257b65668d98ea8b103b6c6be98
rutrum <dave@rutrum.net>
2025-03-22 23:10:23 -0400

    Lots of more optimizations

--- chart_gen_rs/src/bin/search_improved.rs
+++ chart_gen_rs/src/bin/search_improved.rs
@@ -3,7 +3,7 @@ use std::fmt;
 
 #[derive(Debug, Clone)]
 struct PairCount<const SEATS: usize> {
-    counts: [[usize; SEATS]; SEATS],
+    pub counts: [[usize; SEATS]; SEATS],
 }
 
 impl<const SEATS: usize> PairCount<SEATS> {
@@ -81,57 +81,43 @@ fn dfs_loop<const SEATS: usize, const ROUNDS: usize>(
     let mut player: Player = 0;
     let mut round_players = [false; SEATS];
 
-    // update this
-    let mut possible_players = [true; SEATS];
-
     let mut loop_count: u64 = 0;
 
-    // min player per seat
-    // 1 2 2 3    2 3 3 4    3 4 4 5
-
-    let min_player_per_seat: Vec<Player> = (0..SEATS)
-        .map(|seat| match seat % 4 {
-            0 => seat / 4 + 1,
-            1 | 2 => seat / 4 + 2,
-            _ => seat / 4 + 3, // 3
-        } as Player)
-        .collect();
-
-    let max_player_per_seat: Vec<usize> = (0..SEATS)
+    let max_player_per_seat: Vec<Player> = (0..SEATS)
         .map(|seat| match seat {
             0 => 1,
             s if s % 2 == 1 => SEATS,
             s if s % 4 == 2 => SEATS - 1,
-            _ => SEATS - (SEATS / 4 - seat / 4) - 1,
-        })
+            s if s % 4 == 0 => SEATS - (SEATS / 4 - seat / 4) - 1,
+            _ => unreachable!(),
+        } as Player)
         .collect();
 
-    println!("min");
-    min_player_per_seat.iter().for_each(|x| println!("{x}"));
     println!("max");
     max_player_per_seat.iter().for_each(|x| println!("{x}"));
 
+    let mut loop_count_by_seat = [[0_u64; SEATS]; ROUNDS];
+
     loop {
+        // player at this point means "we just checked player"
+        // "we plan on looking at player+1 next"
         loop_count += 1;
+        loop_count_by_seat[round][seat] += 1;
+
         if loop_count % 1_000_000_000 == 0 {
             println!("{loop_count}");
         }
-        //println!("{chart}");
+
+        if loop_count > 10_000_000_000 {
+            break None;
+        }
 
         // what if I could check right here, or just know
         // that a certain player wasn't applicable to this spot
         // what if as I assigned players, I also scatched off seats
         // kind of like round_players...
 
-        // max player value for each seat
-        // 1 12 11 12 9 12 11 12 10 12 11 12
-        // lets try the two lowest fruit
-        //println!("{seat} {player} {seat_max}");
-
-        // try next player
-        if (player as usize) < max_player_per_seat[seat as usize] {
-            player += 1;
-        } else {
+        if player == max_player_per_seat[seat as usize] {
             // that was last player, go to last seat
             assert_eq!(chart.rounds[round][seat], 0);
 
@@ -158,15 +144,14 @@ fn dfs_loop<const SEATS: usize, const ROUNDS: usize>(
             }
             if left > 0 {
                 chart.opponent_counts.dec(player, left);
-            }
-            if right > 0 {
                 chart.opponent_counts.dec(player, right);
             }
 
             continue;
+            // here player can be 1 to 12
         }
 
-        //println!("{loop_count}: {round} {seat} : {player}");
+        player += 1;
 
         // if left has to match player later (opponent count(player, left) = 1)
         // but right can't be an opponent of player nor a partner of left
@@ -182,6 +167,7 @@ fn dfs_loop<const SEATS: usize, const ROUNDS: usize>(
 
         if round_players[player as usize - 1] {
             continue;
+            // here player is 1 to 12
         }
 
         // can I do this calculation in place or one at a time?
@@ -194,8 +180,9 @@ fn dfs_loop<const SEATS: usize, const ROUNDS: usize>(
         // so maybe this conditional should be broken up more
 
         if (partner == 0 || chart.partner_counts.get(player, partner) < 1)
-            && ((left == 0 || chart.opponent_counts.get(player, left) < 2)
-                && (right == 0 || chart.opponent_counts.get(player, right) < 2))
+            && (left == 0
+                || (chart.opponent_counts.get(player, left) < 2)
+                    && chart.opponent_counts.get(player, right) < 2)
         {
             // valid player assignment, go to next seat
             chart.rounds[round][seat] = player;
@@ -207,8 +194,6 @@ fn dfs_loop<const SEATS: usize, const ROUNDS: usize>(
             }
             if left > 0 {
                 chart.opponent_counts.inc(player, left);
-            }
-            if right > 0 {
                 chart.opponent_counts.inc(player, right);
             }
 
@@ -216,36 +201,41 @@ fn dfs_loop<const SEATS: usize, const ROUNDS: usize>(
             if seat < SEATS - 1 {
                 seat += 1;
                 round_players[player as usize - 1] = true;
-            //} else if round < 5 {
+
+                if seat % 2 == 1 {
+                    // the right partner should be greater than the left
+                    player = chart.rounds[round][seat - 1];
+                } else if seat % 4 == 2 {
+                    // the first opponent at a table should be greater than the player
+                    player = chart.rounds[round][seat - 2];
+                } else if seat % 4 == 0 {
+                    // the first person of the next table should be greater than last
+                    player = chart.rounds[round][seat - 4];
+                }
             } else if round < ROUNDS - 1 {
                 seat = 0;
                 round += 1;
                 round_players = [false; SEATS];
+                player = 0;
             } else {
                 // found it!
                 println!("Final count: {loop_count}");
+                for r in loop_count_by_seat {
+                    let s: u64 = r.iter().sum();
+                    println!("{s}: {r:?}");
+                }
                 break Some(chart);
             }
-
-            if seat % 2 == 1 {
-                // the right partner should be greater than the left
-                player = chart.rounds[round][seat - 1];
-            } else if seat % 4 == 2 {
-                // the first opponent at a table should be greater than the player
-                player = chart.rounds[round][seat - 2];
-            } else if seat % 4 == 0 && seat > 0 {
-                // the first person of the next table should be greater than last
-                player = chart.rounds[round][seat - 4];
-            } else {
-                player = min_player_per_seat[seat] - 1;
-            }
         }
+        // What options can player be here?
+        // 0 up to 11
     }
 }
 
 fn main() {
-    let chart = Chart::<8, 7>::new();
-    //let chart = Chart::<12, 11>::new();
+    //let chart = Chart::<8, 7>::new();
+    //let chart = Chart::<12, 6>::new();
+    let chart = Chart::<12, 7>::new();
     let new_chart = dfs_loop(chart).unwrap();
     println!("{new_chart}");
     println!("Partner counts: \n{}", new_chart.partner_counts);
@@ -338,3 +328,56 @@ fn main() {
 // 8 players: now start with min_player_per_seat
 // 13_91821
 // lol I already have better lower bounds set by sorting arguments
+
+// 8 players: look ahead to next pair and make sure its okay
+// 13_21149
+// but I worry this is slower, since I'm doing O(SEATS) check
+// that said, the common case is short circuited, so its not always SEATS
+// checks
+
+// One bench is just to actually make the chart have less rounds.
+// 12 players, 6 rounds
+// 33_74149
+
+// Thought I saw a bug.  The final result ended with the second player
+// being a 10, not a 7.  That's because the first 5 rounds had so much
+// of the same opponents with 1 that it made partnering with 1 and 7
+// inviable for everything.  This is a "lookahead" problem that
+// I could fix.  When I assign 7, I should be able to see if there
+// exist 2 opponents that would work for 7 and 1
+
+// Problem: the case is more narrow than expected.  This scenario is "unlikely"
+// in my testing.  It just happens the case I saw, when valid opponents
+// were available they still failed since those opponents couldn't be
+// partners.  I dont know if I can look ahead without incurring
+// too much cost to time doing loops constantly.
+
+// doing this first check made the first 10bil loops go from 1m to 1m14s
+
+// look ahead checks
+// before assigning seat % 4 == 1, make sure two opponents are viable
+// for the next two slots
+// if seat % 4 == 1 {
+//     let opponents_viable = (1..=SEATS as Player).any(|i| {
+//         chart.opponent_counts.get(player, i) < 2
+//             && chart.opponent_counts.get(partner, i) < 2
+//             && i != player
+//             && i != partner
+//     });
+
+//     if !opponents_viable {
+//         continue;
+//     }
+// }
+//else if seat % 4 == 2 {
+//    // check that partners are available
+//    let partner_available = (1..=SEATS as Player)
+//        .any(|i| chart.partner_counts.get(player, i) < 1 && i != player);
+
+//    if !partner_available {
+//        //continue;
+//    }
+//}
+
+// After removing duplicate checks for left AND right...if left is nonzero, so is right
+// real    0m55.372s
